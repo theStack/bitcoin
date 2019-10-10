3da6aac15a91932c8d1a644e9fdf85e043e9f8ef
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2019-10-10 12:23:10 +0200

    add full bloom filter benchmarks

--- src/bench/rollingbloom.cpp
+++ src/bench/rollingbloom.cpp
@@ -6,6 +6,7 @@
 
 #include <bench/bench.h>
 #include <bloom.h>
+#include <util/strencodings.h>
 
 static void RollingBloom(benchmark::State& state)
 {
@@ -36,5 +37,60 @@ static void RollingBloomReset(benchmark::State& state)
     }
 }
 
+// TODO: move this into separate file bloom.cpp
+static CBloomFilter CreateFullBloomFilter()
+{
+    CBloomFilter filter(10, 0.01, 0, BLOOM_UPDATE_ALL);
+    for (int i = 0; i < 200; i++)
+        filter.insert(std::vector<unsigned char>(1, i));
+    return filter;
+}
+
+static void BloomFull_Regular_Contains(benchmark::State& state)
+{
+    CBloomFilter filter = CreateFullBloomFilter();
+    std::vector<unsigned char> element = ParseHex("affe1234");
+
+    while (state.KeepRunning()) {
+        filter.contains(element);
+    }
+}
+
+static void BloomFull_Regular_Insert(benchmark::State& state)
+{
+    CBloomFilter filter = CreateFullBloomFilter();
+    std::vector<unsigned char> element = ParseHex("affe1234");
+
+    while (state.KeepRunning()) {
+        filter.insert(element);
+    }
+}
+
+static void BloomFull_UpdateEmptyFull_Contains(benchmark::State& state)
+{
+    CBloomFilter filter = CreateFullBloomFilter();
+    std::vector<unsigned char> element = ParseHex("affe1234");
+    filter.UpdateEmptyFull();
+
+    while (state.KeepRunning()) {
+        filter.contains(element);
+    }
+}
+
+static void BloomFull_UpdateEmptyFull_Insert(benchmark::State& state)
+{
+    CBloomFilter filter = CreateFullBloomFilter();
+    std::vector<unsigned char> element = ParseHex("affe1234");
+    filter.UpdateEmptyFull();
+
+    while (state.KeepRunning()) {
+        filter.insert(element);
+    }
+}
+
 BENCHMARK(RollingBloom, 1500 * 1000);
 BENCHMARK(RollingBloomReset, 20000);
+BENCHMARK(BloomFull_Regular_Contains, 21 * 1000000);
+BENCHMARK(BloomFull_Regular_Insert, 21 * 1000000);
+BENCHMARK(BloomFull_UpdateEmptyFull_Contains, 21 * 1000000);
+BENCHMARK(BloomFull_UpdateEmptyFull_Insert, 21 * 1000000);
