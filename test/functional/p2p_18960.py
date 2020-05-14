1d504570d1e090c1e076e8f433e7aabdb50cc1d9
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2020-05-14 18:43:00 +0200

    polish up #18960 test script

--- test/functional/p2p_18960.py
+++ test/functional/p2p_18960.py
@@ -6,31 +6,46 @@ from test_framework.messages import (
     msg_getcfcheckpt,
 )
 from test_framework.util import assert_equal
-import time
+import os, time
 
-# block number 630000
-STOP_HASH = "000000000000000000024bead8df69990852c202db0e0097c1a12ea637d7e96d"
+STOP_BLOCK_HEIGHT = 630000
+NUMBER_OF_RUNS = 100
 
 test = NodeShell()
-test.setup(datadir="/home/honeybadger/.bitcoin/")
-info = test.nodes[0].getnetworkinfo()
-print(info)
-
+test.setup(datadir= os.getenv("HOME") + "/.bitcoin/")
+stop_hash = test.nodes[0].getblockhash(STOP_BLOCK_HEIGHT)
 p2p_conn = test.nodes[0].p2ps[0]
 
-for run in range(1,100+1):
-    request = msg_getcfcheckpt(
-        filter_type=FILTER_TYPE_BASIC,
-        stop_hash=int(STOP_HASH, 16)
-    )
+request = msg_getcfcheckpt(
+    filter_type=FILTER_TYPE_BASIC,
+    stop_hash=int(stop_hash, 16)
+)
+
+first_processing_time = None
+following_processing_times = []
+
+for run in range(1,NUMBER_OF_RUNS+1):
     start_time = time.time()
     p2p_conn.send_and_ping(message=request)
-    end_time = time.time()
+    processing_time = time.time() - start_time
+
     response = p2p_conn.last_message['cfcheckpt']
     assert_equal(response.filter_type, request.filter_type)
     assert_equal(response.stop_hash, request.stop_hash)
-    #assert_equal(len(response.headers), 1)
-    print("run {}: got {} headers in {} seconds".format(run,
-        len(response.headers), end_time-start_time))
+    assert_equal(len(response.headers), STOP_BLOCK_HEIGHT//1000)
+
+    print("run {}: got {} headers in {} ms".format(run,
+        len(response.headers), processing_time*1000))
+    if run == 1:
+        first_processing_time = processing_time
+    else:
+        following_processing_times.append(processing_time)
+
+print("getcfcheckpt request processing times")
+print("=====================================")
+print("-> first:     {:.2f}ms".format(first_processing_time*1000))
+average_processing_time = sum(following_processing_times)/len(following_processing_times)
+print("-> following: {:.2f}ms (average)".format(average_processing_time*1000))
+print("=====================================")
 
 test.shutdown()
