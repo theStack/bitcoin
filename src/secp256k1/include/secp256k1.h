d6646237695154321fcd35b539a97bf0609858ae
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2024-03-14 17:21:20 +0100

    secp256k1: add scalar internals PoC functions (taken from secp256k1-zkp)
    note that this is intentionally added to the main module (secp256k1.{h,c}) in
    order to keep things simple and avoid bothering with the build system
    
    source: https://github.com/BlockstreamResearch/secp256k1-zkp/pull/153

--- src/secp256k1/include/secp256k1.h
+++ src/secp256k1/include/secp256k1.h
@@ -902,6 +902,20 @@ SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_tagged_sha256(
     size_t msglen
 ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(5);
 
+
+/*****************************************************/
+/** INTERNALS / HAZMAT functions (proof-of-concept) **/
+/*****************************************************/
+
+/* Caller is responsible for allocating a maximally-aligned space of size
+   secp256k1_internals_scalar_size */
+typedef struct secp256k1_internals_scalar_struct secp256k1_internals_scalar;
+
+SECP256K1_API size_t secp256k1_internals_scalar_size(void);
+SECP256K1_API void secp256k1_internals_scalar_set_b32(secp256k1_internals_scalar *r, const unsigned char *bin, int *overflow);
+SECP256K1_API void secp256k1_internals_scalar_get_b32(unsigned char *bin, const secp256k1_internals_scalar* a);
+SECP256K1_API int secp256k1_internals_scalar_add(secp256k1_internals_scalar *r, const secp256k1_internals_scalar *a, const secp256k1_internals_scalar *b);
+
 #ifdef __cplusplus
 }
 #endif
--- src/secp256k1/src/secp256k1.c
+++ src/secp256k1/src/secp256k1.c
@@ -815,3 +815,28 @@ int secp256k1_tagged_sha256(const secp256k1_context* ctx, unsigned char *hash32,
 #ifdef ENABLE_MODULE_ELLSWIFT
 # include "modules/ellswift/main_impl.h"
 #endif
+
+
+/*****************************************************/
+/** INTERNALS / HAZMAT functions (proof-of-concept) **/
+/*****************************************************/
+
+struct secp256k1_internals_scalar_struct {
+    secp256k1_scalar s;
+};
+
+size_t secp256k1_internals_scalar_size() {
+    return sizeof(struct secp256k1_internals_scalar_struct);
+}
+
+void secp256k1_internals_scalar_set_b32(secp256k1_internals_scalar *r, const unsigned char *bin, int *overflow) {
+    secp256k1_scalar_set_b32(&r->s, bin, overflow);
+}
+
+void secp256k1_internals_scalar_get_b32(unsigned char *bin, const secp256k1_internals_scalar* a) {
+    secp256k1_scalar_get_b32(bin, &a->s);
+}
+
+int secp256k1_internals_scalar_add(secp256k1_internals_scalar *r, const secp256k1_internals_scalar *a, const secp256k1_internals_scalar *b) {
+    return secp256k1_scalar_add(&r->s, &a->s, &b->s);
+}
