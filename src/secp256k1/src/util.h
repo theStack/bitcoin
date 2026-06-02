494a4734c408ebb78802dc8bf25836ed5dc4470f
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2026-06-02 02:07:26 +0200

    apply secp256k1 PR #1859 ("field: force-inline 5x52 mul and sqr") to subtree

--- src/secp256k1/src/util.h
+++ src/secp256k1/src/util.h
@@ -57,6 +57,14 @@ static void print_buf_plain(const unsigned char *buf, size_t len) {
 #  define SECP256K1_INLINE inline
 # endif
 
+# if defined(_MSC_VER)
+#  define SECP256K1_ALWAYS_INLINE __forceinline
+# elif SECP256K1_GNUC_PREREQ(3, 0)
+#  define SECP256K1_ALWAYS_INLINE SECP256K1_INLINE __attribute__((always_inline))
+# else
+#  define SECP256K1_ALWAYS_INLINE SECP256K1_INLINE
+# endif
+
 /** Assert statically that expr is true.
  *
  * This is a statement-like macro and can only be used inside functions.
